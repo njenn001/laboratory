12af7e0582bc9ffd357d9db66971a21875f35954
njenning <njenn001@odu.edu>
2023-04-03 20:02:51 -0400

    Frontend expansion 0.0.7

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -36,6 +36,18 @@ def throw_exec(msg):
     elif 'quickstart' in msg: 
         os.system('echo quickstart error')
         return render_template('404.html'), 404
+    elif 'ap' in msg: 
+        os.system('echo ap error')
+        return render_template('404.html'), 404
+    elif 'server' in msg: 
+        os.system('echo server error')
+        return render_template('404.html'), 404
+    elif 'clients' in msg: 
+        os.system('echo clients error')
+        return render_template('404.html'), 404
+    elif 'comms' in msg: 
+        os.system('echo comms error')
+        return render_template('404.html'), 404
 
 """ Sets the rules for the server. 
 
@@ -48,8 +60,11 @@ def set_rules(app):
     app.add_url_rule('/info/background', endpoint='background')
     app.add_url_rule('/info/dependencies', endpoint='dependencies')
     app.add_url_rule('/info/quickstart', endpoint='quickstart')
-     
-
+    app.add_url_rule('/info/ap', endpoint='ap')
+    app.add_url_rule('/info/server', endpoint='server')
+    app.add_url_rule('/info/clients', endpoint='clients')
+    app.add_url_rule('/info/comms', endpoint='comms')
+    
 """ Sets the error handlers on the server. 
 
 @param app: The Flask application
@@ -73,6 +88,10 @@ def set_methods(app):
     background_methods = ['GET']
     dependencies_methdos = ['GET']
     quickstart_methods = ['GET', 'POST']
+    ap_methods=['GET']
+    server_methods=['GET']
+    clients_methods=['GET']
+    comms_methods=['GET']
 
     """ Defines the main route of the server.
     "localhost:5000/"
@@ -158,7 +177,74 @@ def set_methods(app):
                 return render_template('info/quickstart.html',style=url_for('static', filename='info/quickstart.css'))
         except Exception as ex: 
             return throw_exec('quickstart')
-        
+                
+    """ Defines the ap route. 
+    "localhost:5000/info/ap"
+
+    @return render_template() : 
+    @rtype render_template() : template 
+    """
+    @app.route('/info/ap', methods = ap_methods)
+    def ap():
+        try: 
+            """ Check for arguments. """
+            if (request.method == 'GET'): 
+                os.system('echo web user')
+                return render_template('info/ap.html',style=url_for('static', filename='info/ap.css'))
+        except Exception as ex: 
+            return throw_exec('ap')
+                
+    """ Defines the server route. 
+    "localhost:5000/info/server"
+
+    @return render_template() : 
+    @rtype render_template() : template 
+    """
+    @app.route('/info/server', methods = server_methods)
+    def server(): 
+
+        try: 
+            """ Check for arguments. """
+            if (request.method == 'GET'): 
+                os.system('echo web user')
+                return render_template('info/server.html',style=url_for('static', filename='info/server.css'))
+        except Exception as ex: 
+            return throw_exec('server')
+                
+    """ Defines the client route. 
+    "localhost:5000/info/client"
+
+    @return render_template() : 
+    @rtype render_template() : template 
+    """
+    @app.route('/info/clients', methods = clients_methods)
+    def clients(): 
+
+        try: 
+            """ Check for arguments. """
+            if (request.method == 'GET'): 
+                os.system('echo web user')
+                return render_template('info/clients.html',style=url_for('static', filename='info/clients.css'))
+        except Exception as ex: 
+            return throw_exec('clients')
+            
+    """ Defines the comms route. 
+    "localhost:5000/info/comms"
+
+    @return render_template() : 
+    @rtype render_template() : template 
+    """
+    @app.route('/info/comms', methods = comms_methods)
+    def comms(): 
+
+        try: 
+            """ Check for arguments. """
+            if (request.method == 'GET'): 
+                os.system('echo web user')
+                return render_template('info/comms.html',style=url_for('static', filename='info/comms.css'))
+        except Exception as ex: 
+            return throw_exec('comms')    
+    
 """ Sets the app instance. 
 
 @param app: The Flask application
